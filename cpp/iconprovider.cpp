64c6f9331500dbc97ed5f4118653d3c64065d0c7
Marius Cirsta <mforce2@gmail.com>
2019-03-10 20:40:11 +0200

    remove Android stuff and switch build to cmake

--- cpp/iconprovider.cpp
+++ cpp/iconprovider.cpp
@@ -2,11 +2,6 @@
 
 #include <QDebug>
 
-#if defined(Q_OS_ANDROID)
-#include <QtAndroidExtras>
-#include <QtAndroid>
-#endif
-
 ImageProvider::ImageProvider()
     : QQuickImageProvider(QQuickImageProvider::Image)
 {
@@ -23,33 +18,8 @@ QImage ImageProvider::getApplicationIcon(const QString &packageName)
 {
     QImage image;
 
-#ifdef Q_OS_ANDROID
-    QAndroidJniObject appIcon = QtAndroid::androidActivity().callObjectMethod("getApplicationIcon",
-    "(Ljava/lang/String;)[B",
-    QAndroidJniObject::fromString(packageName).object<jstring>());
-
-    QAndroidJniEnvironment env;
-    jbyteArray iconDataArray = appIcon.object<jbyteArray>();
-
-    if (!iconDataArray)
-    {
-        qWarning() << Q_FUNC_INFO << "No icon data";
-
-        return image;
-    }
-
-    jsize iconSize = env->GetArrayLength(iconDataArray);
-
-    if (iconSize > 0)
-    {
-        jbyte *icon = env->GetByteArrayElements(iconDataArray, 0);
-        image = QImage(QImage::fromData((uchar *)icon, iconSize, "PNG"));
-        env->ReleaseByteArrayElements(iconDataArray, icon, JNI_ABORT);
-    }
-#else
     Q_UNUSED(packageName);
     image = QImage("://icon/application.png");
 
-#endif
     return image;
 }
